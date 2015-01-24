b1678dcfee81100b3e385c3260929bc8a6df0f1b
Laszlo Csontos <csontos.blaszlo@gmail.com>
2015-01-24 16:40:10 +0100

    Fix API and add test cases

--- src/github.com/lcsontos/uwatch/webservice/webservice.go
+++ src/github.com/lcsontos/uwatch/webservice/webservice.go
@@ -20,17 +20,32 @@ type InvalidVideoUrl struct {
 	VideoUrl string
 }
 
+type ParsedVideoUrl struct {
+	videoId   string
+	videoType VideoType
+}
+
+type LengthenVideoUrl struct {
+	ParsedVideoUrl
+	urlId   int64
+	urlPath string
+}
+
 func (err *InvalidVideoUrl) Error() string {
 	return fmt.Sprintf("\"%s\" is an invalid video URL", err.VideoUrl)
 }
 
-func LongVideoUrl(videoType VideoType, videoId string) (string, error) {
-	return "", nil
+func LongVideoUrl(videoType VideoType, videoId string) (*LengthenVideoUrl, error) {
+	return nil, nil
 }
 
-func ParseVideoUrl(videoUrl string) (VideoType, string, error) {
-	return YouTube, "", nil
+func ParseVideoUrl(videoUrl string) (*ParsedVideoUrl, error) {
+	return nil, nil
 }
 
 func ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+}
+
+func (url *LengthenVideoUrl) String() string {
+	return ""
 }
--- src/github.com/lcsontos/uwatch/webservice/webservice_test.go
+++ src/github.com/lcsontos/uwatch/webservice/webservice_test.go
@@ -11,13 +11,36 @@ type longVideoUrlTestCase struct {
 }
 
 type parseVideoUrlTestCase struct {
-	want     string
+	want     *ParsedVideoUrl
 	videoUrl string
 }
 
-var longVideoUrlData = []longVideoUrlTestCase{}
+const (
+	_EMPTY             = ""
+	_INVALID_VIDEO_ID  = "abcdefgh"
+	_INVALID_VIDEO_URL = "http://www.google.com"
+)
 
-var parseVideoUrlData = []parseVideoUrlTestCase{}
+var longVideoUrlData = []longVideoUrlTestCase{
+	{"meditations-with-sri-chinmoy-vol--1", YouTube, "5VAYzfvNI1w"},
+	{"zen---musica-de-relajacion-y-balance-espiritual", YouTube, "Gd0TiO0iMfc"},
+	{_EMPTY, YouTube, _EMPTY},
+	{_EMPTY, Vimeo, _EMPTY},
+	{_EMPTY, Youku, _EMPTY},
+	{_EMPTY, Rutube, _EMPTY},
+	{_EMPTY, YouTube, _INVALID_VIDEO_ID},
+	{_EMPTY, Vimeo, _INVALID_VIDEO_ID},
+	{_EMPTY, Youku, _INVALID_VIDEO_ID},
+	{_EMPTY, Rutube, _INVALID_VIDEO_ID},
+}
+
+var parseVideoUrlData = []parseVideoUrlTestCase{
+	{&ParsedVideoUrl{"Gd0TiO0iMfc", YouTube}, "https://www.youtube.com/watch?v=Gd0TiO0iMfc"},
+	{&ParsedVideoUrl{"Gd0TiO0iMfc", YouTube}, "http://youtu.be/Gd0TiO0iMfc"},
+	{&ParsedVideoUrl{"5VAYzfvNI1w", YouTube}, "https://www.youtube.com/watch?v=5VAYzfvNI1w"},
+	{&ParsedVideoUrl{"5VAYzfvNI1w", YouTube}, "http://youtu.be/5VAYzfvNI1w"},
+	{nil, _INVALID_VIDEO_URL},
+}
 
 func TestLongVideoUrl(t *testing.T) {
 
