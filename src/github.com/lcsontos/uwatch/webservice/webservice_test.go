2e4a0ed27891de30d2602fa7060a680cce5de61d
Laszlo Csontos <csontos.blaszlo@gmail.com>
2015-01-24 17:04:46 +0100

    Implement test cases

--- src/github.com/lcsontos/uwatch/webservice/webservice_test.go
+++ src/github.com/lcsontos/uwatch/webservice/webservice_test.go
@@ -1,6 +1,7 @@
 package webservice
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -43,9 +44,47 @@ var parseVideoUrlData = []parseVideoUrlTestCase{
 }
 
 func TestLongVideoUrl(t *testing.T) {
+	for _, data := range longVideoUrlData {
+		result, err := LongVideoUrl(data.videoType, data.videoId)
 
+		if err != nil && data.want != _EMPTY {
+			t.Fatal(err)
+		}
+
+		got := _EMPTY
+
+		if result != nil {
+			got = result.urlPath
+		}
+
+		want := data.want
+
+		if got != want {
+			t.Fatalf("TestLongVideoUrl(%v, %v).urlPath = %v, but wanted %v",
+				data.videoType, data.videoId, got, want)
+		}
+	}
 }
 
 func TestParseVideoUrl(t *testing.T) {
+	for _, data := range parseVideoUrlData {
+		result, err := ParseVideoUrl(data.videoUrl)
 
+		if err != nil && data.want != nil {
+			t.Fatal(err)
+		}
+
+		got := ParsedVideoUrl{}
+
+		if result != nil {
+			got = *result
+		}
+
+		want := *data.want
+
+		if !reflect.DeepEqual(got, want) {
+			t.Fatalf("TestParseVideoUrl(%v) = %v, but wanted %v",
+				data.videoUrl, got, want)
+		}
+	}
 }
