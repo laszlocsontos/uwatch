37734bf3d408ffbcf3b044294c51084b441bf5cc
Laszlo Csontos <csontos.blaszlo@gmail.com>
2015-01-25 18:58:33 +0100

    Implement string normalizer

--- src/github.com/lcsontos/uwatch/normalizer/normalizer_test.go
+++ src/github.com/lcsontos/uwatch/normalizer/normalizer_test.go
@@ -4,6 +4,35 @@ import (
 	"testing"
 )
 
-func TestNormalize(t *testing.T) {
+type testCase struct {
+	want string
+	text string
+}
 
+var testCaseData = []testCase{
+	{
+		"zen---musica-de-relajacion-y-balance-espiritual",
+		"ZEN - MÚSICA DE RELAJACIÓN Y BALANCE ESPIRITUAL",
+	},
+	{
+		"meditations-with-sri-chinmoy-vol--1",
+		"Meditations with Sri Chinmoy Vol. 1",
+	},
+	{
+		"ouooueaui",
+		"öüóőúéáűí",
+	},
+}
+
+func TestNormalize(t *testing.T) {
+	for _, data := range testCaseData {
+		text := data.text
+
+		got, want := Normalize(text), data.want
+
+		// TODO manual test succeeds, but this fails, check later why?
+		if got != want {
+			t.Fatalf("Normalize(%s) = %s, but wanted %s", text, got, want)
+		}
+	}
 }
