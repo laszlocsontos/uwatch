10655a1d468aedcab0d02c70d271c561c4a6a4ba
Laszlo Csontos <csontos.blaszlo@gmail.com>
2015-01-25 17:35:18 +0100

    Removed code duplication

--- src/github.com/lcsontos/uwatch/util/util.go
+++ src/github.com/lcsontos/uwatch/util/util.go
@@ -17,15 +17,23 @@ func HandleError(rw http.ResponseWriter, req *http.Request, err, apperr error, i
 	} else {
 		status = http.StatusInternalServerError
 
-		// TODO Generalize error handling
-		http.Error(rw, fmt.Sprintf("INTERNAL ERROR: %s", err.Error()), status)
-		log.Printf(err.Error())
+		logError(err, req)
+
+		msg := fmt.Sprintf("INTERNAL ERROR: %s", err.Error())
+
+		http.Error(rw, msg, status)
 	}
 
 	return true
 }
 
 func HandlePanic(err interface{}, rw http.ResponseWriter, req *http.Request) {
+	logError(err, req)
+
+	http.Error(rw, "SYSTEM ERROR", http.StatusInternalServerError)
+}
+
+func logError(err interface{}, req *http.Request) {
 	var stack [4096]byte
 
 	runtime.Stack(stack[:], false)
@@ -33,6 +41,4 @@ func HandlePanic(err interface{}, rw http.ResponseWriter, req *http.Request) {
 	log.Printf(
 		"Handler for %s[url:%s, data:%s] has failed with %s\nStack trace:\n %s\n",
 		req.Method, req.URL, req.Form, err, stack[:])
-
-	http.Error(rw, "SYSTEM ERROR", http.StatusInternalServerError)
 }
