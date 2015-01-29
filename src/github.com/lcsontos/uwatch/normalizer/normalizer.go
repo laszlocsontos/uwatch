90863972df96c7b443f8e4afcc965d122f3a55cc
Laszlo Csontos <csontos.blaszlo@gmail.com>
2015-01-29 21:36:54 +0100

    Fix normalizer

--- src/github.com/lcsontos/uwatch/normalizer/normalizer.go
+++ src/github.com/lcsontos/uwatch/normalizer/normalizer.go
@@ -19,6 +19,7 @@ package normalizer
 
 import (
 	"strings"
+	"unicode/utf8"
 )
 
 func Normalize(text string) string {
@@ -26,38 +27,41 @@ func Normalize(text string) string {
 		return text
 	}
 
-	var normalized = make([]rune, len(text))
-
 	text = strings.ToLower(text)
 
-	for index, runeValue := range text {
-		char := runeValue
+	var normalized = make([]rune, 0)
+
+	for index, width := 0, 0; index < len(text); index += width {
+		var newRuneValue, oldRuneValue rune
+
+		oldRuneValue, width = utf8.DecodeRuneInString(text[index:])
 
 		switch {
-		case runeValue >= '0' && runeValue <= '9':
-			normalized[index] = char
-		case runeValue >= 'a' && runeValue <= 'z':
-			normalized[index] = char
-		case runeValue >= 224 && runeValue <= 229:
-			normalized[index] = 'a'
-		case runeValue >= 232 && runeValue <= 235:
-			normalized[index] = 'e'
-		case runeValue >= 236 && runeValue <= 239:
-			normalized[index] = 'i'
-		case runeValue == 241:
-			normalized[index] = 'n'
-		case (runeValue >= 242 && runeValue <= 246) || (runeValue == 337):
-			normalized[index] = 'o'
-		case (runeValue >= 249 && runeValue <= 252) || (runeValue == 369):
-			normalized[index] = 'u'
-		case runeValue == 253:
-			normalized[index] = 'y'
-		case runeValue == 255:
-			normalized[index] = 'y'
+		case oldRuneValue >= '0' && oldRuneValue <= '9':
+			newRuneValue = oldRuneValue
+		case oldRuneValue >= 'a' && oldRuneValue <= 'z':
+			newRuneValue = oldRuneValue
+		case oldRuneValue >= 224 && oldRuneValue <= 229:
+			newRuneValue = 'a'
+		case oldRuneValue >= 232 && oldRuneValue <= 235:
+			newRuneValue = 'e'
+		case oldRuneValue >= 236 && oldRuneValue <= 239:
+			newRuneValue = 'i'
+		case oldRuneValue == 241:
+			newRuneValue = 'n'
+		case (oldRuneValue >= 242 && oldRuneValue <= 246) || (oldRuneValue == 337):
+			newRuneValue = 'o'
+		case (oldRuneValue >= 249 && oldRuneValue <= 252) || (oldRuneValue == 369):
+			newRuneValue = 'u'
+		case oldRuneValue == 253:
+			newRuneValue = 'y'
+		case oldRuneValue == 255:
+			newRuneValue = 'y'
 		default:
-			normalized[index] = '-'
+			newRuneValue = '-'
 		}
 
+		normalized = append(normalized, newRuneValue)
 	}
 
 	return string(normalized)
